c34cfafb708fe5768094075c54e749ecb862e9a7
lailata <93602827+lailata@users.noreply.github.com>
2024-01-22 16:04:11 +0100

    Push model.js

---
+++ model.js
@@ -0,0 +1,21 @@
+/* Draft */
+
+const pg = require("pg");
+
+class DB {
+  constructor() {
+    this.pg = new pg.Pool({
+      user: "your_username",
+      host: "your_host",
+      database: "your_databse",
+      password: "your_password",
+      port: 5432, //Default port
+    });
+  }
+  async getUserFromUsername(username) {
+    const query = "SELECT * FROM users WHERE username = $1";
+    const values = [username];
+    const result = await this.pg.query(query, values);
+    return result.rows[0];
+  }
+}
