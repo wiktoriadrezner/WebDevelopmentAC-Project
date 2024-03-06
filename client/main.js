67bd5915f24ea86a45752a8197193f3ebaeb5f9f
Wiktoria Drezner <93138210+wiktoriadrezner@users.noreply.github.com>
2024-03-06 11:44:05 +0100

    Clean v2

--- client/main.js
+++
@@ -1,8 +0,0 @@
-fetch("http://localhost:3000/stores/all")
-    .then((response) => response.json())
-    .then(function (stores) {
-        console.log(stores);
-        // That's where we manipulate the data
-    });
-
-console.log("test");
